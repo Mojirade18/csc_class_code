414ce4f8775ab2f865959b90fca079746d9fad2a
Olumide Adekanmi <115383380+upmide@users.noreply.github.com>
2024-12-29 08:10:32 +0100

    I added some new things i recapped for my python

---
+++ Rekindling_py_code.py
@@ -0,0 +1,21 @@
+# This is the first code of print
+print("What do you do for a living?")
+# This is what you use to print your python version
+import sys
+print(sys.version)
+# Indentation mistakes
+money = time = family = int(0)
+# This is declaration of multiple varaibles
+money, time, family = 0, 0, 0
+'''if money > 5:
+    print("Everyone would always want to be your friend at all cost.")
+        print("It takes a lot.")
+        '''
+# A-Dewine is different from a-dewine and it cant be used as a variable name becase of the -
+
+# To unpack a list
+listing = [9, 8, 7]
+x, y, z = listing
+print(x)
+print(y)
+print(z)
