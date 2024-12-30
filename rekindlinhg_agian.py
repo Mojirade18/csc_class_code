d0da40346eac770005598d0a6de763dc9f3570f1
Olumide Adekanmi <115383380+upmide@users.noreply.github.com>
2024-12-30 06:40:51 +0100

    I did a project rekinling again.

---
+++ rekindlinhg_agian.py
@@ -0,0 +1,13 @@
+# I am trying to cast an integer to a string
+much = int(input("How many family do you have in lagos?"))
+number = str(much)
+print("wow", number, "is a large amount.")
+# This entails manipulattion of strings
+name = input("What's your name? ")
+first = name[0]
+length_of_name = len(name)
+print(first, "is a beautiful letter to start your name.")
+if 'a' in name:
+    print("You are welcome into the A's family")
+for you in name:
+    print(you)
