84117d7e46f49afe5d30e98733b8b49c1aaef7ed
Olumide Adekanmi <115383380+upmide@users.noreply.github.com>
2024-12-31 02:58:36 +0100

    I did a project self made.

--- self_made.py
+++ self_made.py
@@ -4,12 +4,13 @@
 print("Hello there!")
 print("I trust your day is actively going well")
 name = input("Whats your name? ")
+lowercase_name = name.lower()
 age = int(input("How old are you: "))
 gender = input("Are you a boy or a girl")
 print("What a beautiful name for a", age, "years old", gender)
 list_of_a_students = []
-if "a" in name:
-    list_of_a_students.append(name)
+if "a" in lowercase_name:
+    list_of_a_students.append(lowercase_name)
     print("You have just been added to the list of those with a in their names.")
     print("Congratulations once again.")
 if "a" not in name:
