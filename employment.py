a754fc74dae15ce72e6499ac494513261d536c8f
Olumide Adekanmi <115383380+upmide@users.noreply.github.com>
2025-01-01 11:55:04 +0100

    I did a project self made.

---
+++ employment.py
@@ -0,0 +1,15 @@
+# This snippet is to evaluate a user's age and income to provide insights about their statuS and financial stability
+print("Hello there!")
+age = int(input("How old are you? "))
+income = int(input("What is the approximate amount you earn monthly? "))
+if age >= 18 and income > 500000:
+    print("You are eligible to work.")
+elif age < 18 and income <= 500000:
+    print("I think you would mostlikely get broke soon")
+    print("so dont just bother working")
+elif age >= 18 and income < 500000:
+    print("You are eligible to work but you need to find a better job")
+elif age < 18 and income > 500000:
+    print("You dey do yahoo?")
+else:
+    print("wrong input")
