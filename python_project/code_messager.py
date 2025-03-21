ec27f703f399ce004baa5d3c0bda525d659bbdf9
Mojirade18 <mojiradeoluranti@gmail.com>
2025-03-21 12:59:12 +0100

    I added a new code that encrypts messages

--- python_project/code_messager.py
+++ python_project/code_messager.py
@@ -1,4 +1,17 @@
+import random
 Greeting_message = """Hello welcome to message coder.
 This encrypt your message to your friends.
 Enjoy the ride!"""
 print(Greeting_message)
+print("What do you want to do?")
+print("1. encrypt your message.")
+print("2. De-encrypt your message.")
+choice = int(input("What is your choice 1/2: "))
+
+if choice == 1:
+    word = input("Type in the word you want to encrypt: ")
+    for a in word:
+        if a.isalpha():
+            a = random.randrange(1,100)
+            print(a)
+            
