c1255910e5e317d7941cf73106b4cff7412ff7ea
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-09 09:02:41 +0100

    i added a new file

--- python_project/new.py
+++ python_project/new.py
@@ -2,3 +2,8 @@ class student():
     def __init__(self, name, grade):
         self.name = name
         self.grade = grade
+    def __str__(self):
+        return f"{self.name} said school is fun."
+    
+moji_object = student(Moji, 100)
+print(moji_object)
