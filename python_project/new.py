41dced46b46e37afdfe2c5827d863422284004f9
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-09 09:16:55 +0100

    i added a new file

--- python_project/new.py
+++ python_project/new.py
@@ -4,11 +4,19 @@ class Student:
         self.grade = grade
 
     def __str__(self):
-        return f"{self.name} said school is fun."
+        return f"{self.name} 📚 says: 'School is fun!' 🎉"
 
 class Moji(Student):
-    def __init__(self, name, grade):
+    def __init__(self, name, grade, favorite_subject):
         super().__init__(name, grade)
+        self.favorite_subject = favorite_subject
 
-moji_object = Moji("Moji", 100)
+    def __str__(self):
+        return (
+            f"{self.name} 🤓 says: 'Learning is awesome!' 💡\n"
+            f"Grade: {self.grade} 🏆\n"
+            f"Favorite subject: {self.favorite_subject} ❤️"
+        )
+
+moji_object = Moji("Moji", 100, "Physics")
 print(moji_object)
