3fcab1e63a1f840d104386cc0b04a16257475589
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-09 09:10:10 +0100

    i added a new file

--- python_project/new.py
+++ python_project/new.py
@@ -1,9 +1,14 @@
-class student():
+class Student:
     def __init__(self, name, grade):
         self.name = name
         self.grade = grade
+
     def __str__(self):
         return f"{self.name} said school is fun."
-    
-moji_object = student(Moji, 100)
+
+class Moji(Student):
+    def __init__(self, name, grade):
+        super().__init__(name, grade)
+
+moji_object = Moji("Moji", 100)
 print(moji_object)
