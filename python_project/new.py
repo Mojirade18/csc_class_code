bd46f41f51b0591bcc81f4d248066943c648a729
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-10 07:13:31 +0100

    i edited a new file

--- python_project/new.py
+++ python_project/new.py
@@ -1,10 +1,13 @@
 class person(): 
-    def __init__(self, name, age, gender ):
+    def __init__(self, name, age, gender, DOB ):
         self.name = name
         self.age = age
         self.gender = gender
+        self.DOB = DOB
     
     def introduce(self):
         return f"Hey there my name is {self.name}\ni am {self.age} years old."
-    def birthday():
-        
+    def birthday(self):
+        print(f"Since i am {self.age}, then you should probably get my age right")
+        print(f"I was born {2025 - self.age}")
+        print("Therefore my birthday is ")
