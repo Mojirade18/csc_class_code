f6359f2c2bd2d3ca87fff8b2b783afe6ca5b2941
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-11 06:50:05 +0100

    i edited bank.py

--- python_project/bank.py
+++ python_project/bank.py
@@ -2,12 +2,21 @@ class loan:
     def __init__(self, name, account_number, amount):
         self.account_number = account_number
         self.name = name
-        self.account_number
+        self.amount = amount
 
     def accept(self, AIB):
         if AIB > 20000:
-            print("You are eligible to loan money")
+            return("✅You are eligible to loan money")
         else:
-            print("You have to have above 20 000 naira in your account")
+            return("❌You have to have above 20 000 naira in your account")
 
-        
+    def __str__(self):
+        return f"Hey {self.name}, welcome to loany bank app."
+
+
+while True:
+    name = input("What is your name? ") 
+    account_number = input("What is your account number? ")
+    AIB = float(input("How much do you have in your bank account? "))
+    user_loan = loan (name, account_number, AIB)
+    print(user_loan)
