40cc4942d2cf358ed3dd4f4a1edaf129fcce18e1
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-11 06:29:46 +0100

    i edited bank.py

---
+++ python_project/bank.py
@@ -0,0 +1,13 @@
+class loan:
+    def __init__(self, name, account_number, amount):
+        self.account_number = account_number
+        self.name = name
+        self.account_number
+
+    def accept(self, AIB):
+        if AIB > 20000:
+            print("You are eligible to loan money")
+        else:
+            print("You have to have above 20 000 naira in your account")
+
+        
