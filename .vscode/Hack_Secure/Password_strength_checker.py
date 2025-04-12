9702975a42312931cfebaf565800b05bf12d6861
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-12 12:34:48 +0100

    I added a new file called password_strength

--- .vscode/Hack_Secure/Password_strength_checker.py
+++ .vscode/Hack_Secure/Password_strength_checker.py
@@ -2,17 +2,20 @@ def length(password):
     length_of_pass = 0
     for x in password:
         length_of_pass += 1
-    if length_of_pass < range(8, 127):
+    if length_of_pass < 8:
         print("Please increase the length of your password.\nThank you")
+    else:
+        print("Your password has a good length\nWeldon")
 def uppercase():
     pass
 def lowercase():
     pass
+
 def number():
     pass
 def special_character():
     pass
 
 print ("Hey there this is a password checker")
-password = input("Input you password: ")
+password = input("Input you password: ").strip()
 length(password)
