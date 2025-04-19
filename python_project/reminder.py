27fd2f48f7fa018bcb9a4cb5bb18db46def8db47
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-19 21:11:35 +0100

    Edted a file called reminder.py

--- python_project/reminder.py
+++ python_project/reminder.py
@@ -1,12 +1,15 @@
 class Task:
-    def __init__(self, name, time, notes, location):
-        self.name = name
+    total_tasks = 0
+    def __init__(self, time, notes, location):
         self.time = time
         self.notes = notes
         self.location = location
         self.number = 0
-        self.number += 1
+        Task.total_tasks += 1
 
     def __str__(self):
         total = self.number
-        return f"The total amount of task is {self.number}"
+        return f"The total amount of task is {Task.total_tasks}"
+
+class Prep_Tasks(Task):
+    super().__init__()
