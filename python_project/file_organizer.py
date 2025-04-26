a6058ed74c2dd577312f5d815cf1204e47b73c0a
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-26 22:17:10 +0100

    I edited file_organizer.py

--- python_project/file_organizer.py
+++ python_project/file_organizer.py
@@ -1,8 +1,15 @@
-class file:
-    def __init__(self, filename, type):
+
+class File:
+    def __init__(self, filename, file_type):
         self.filename = filename
-        self.type = type
+        self.file_type = file_type
+
     def __str__(self):
-        return f"File: {self.filename}, Type: {self.type}"
-    
-location = input("What is the location of your file: ")
+        return f"File: {self.filename}, Type: {self.file_type}"
+
+
+def organize_files():
+    # Get user input for file locations
+    location = input("What is the location of your file: ").strip()
+    mp_three_file = input("What location do you want your MP3 files to be in: ").strip()
+    mp_four_file = input("What location do you want your MP4 files to be in: ").strip()
