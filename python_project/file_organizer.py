316ca3514b4ee8edeee58fc87cc56924f5cc635f
Mojirade18 <mojiradeoluranti@gmail.com>
2025-04-26 22:08:32 +0100

    I edited file_organizer.py

--- python_project/file_organizer.py
+++ python_project/file_organizer.py
@@ -2,3 +2,7 @@ class file:
     def __init__(self, filename, type):
         self.filename = filename
         self.type = type
+    def __str__(self):
+        return f"File: {self.filename}, Type: {self.type}"
+    
+location = input("What is the location of your file: ")
