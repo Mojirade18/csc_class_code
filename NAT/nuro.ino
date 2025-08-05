951582904d1e9ec0ada9554388bef18bc894587b
Mojirade18 <mojiradeoluranti@gmail.com>
2025-08-05 17:45:51 +0100

    Added the pins values

---
+++ NAT/nuro.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+    int led = 4;
+}
+
+void loop()
+{
+    if topic
+        == Biology;
+    color = setcolor(255, 0, 0);
+    digitalWrite(led, color)
+}
+
+void setcolor(int r, int g, int b)
+{
+    // Function to set color
+    analogWrite(ledPinR, r);
+    analogWrite(ledPinG, g);
+    analogWrite(ledPinB, b);
+}
