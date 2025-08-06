97a3592f2ea803f663b9864929e541a0423222c8
Mojirade18 <mojiradeoluranti@gmail.com>
2025-08-06 15:12:25 +0100

    Added the pins values

--- NAT/nuro.ino
+++ NAT/nuro.ino
@@ -1,6 +1,10 @@
+int ledPinR = 5;
+int ledPinG = 6;
+int ledPinB = 7;
+
 void setup()
 {
-    int led = 4;
+    pinMode(ledPinR, )
 }
 
 void loop()
@@ -8,7 +12,14 @@ void loop()
     if topic
         == Biology;
     color = setcolor(255, 0, 0);
-    digitalWrite(led, color)
+    digitalWrite(led, color) else if topic == Chemistry;
+    color = setcolor(0, 255, 0);
+    digitalWrite(led, color);
+    else if topic == Physics;
+    color = setcolor(0, 0, 255);
+    digitalWrite(led, color);
+    else;
+    print("Topic not recognized");
 }
 
 void setcolor(int r, int g, int b)
